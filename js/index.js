a7a95c6eafb0d03757024f1ae406621980dd1e5f
Dennis Kamunya <dennismuriithik@gmail.com>
2020-08-03 15:38:23 +0300

    Added fntionality to get pizzaform data

--- js/index.js
+++ js/index.js
@@ -5,4 +5,38 @@ $(document).ready(()=>{
   $('#to-be-picked').click(()=>{
     $('#location-hide').hide()
   })
+
+  $('#pizza-form').submit((event)=>{
+    event.preventDefault()
+    
+    let pizzaQuantity=Number($('#pizza-quantity').val())
+    let pizzaSize=$('#pizza-size').val()
+    let pizzaCrust=$('#pizza-crust').val()
+    let pizzaToppings=$('#pizza-toppings').val() || []
+    let delOpt=$('#to-be-delivered').is(":checked");
+    let pckOpt=$('#to-be-picked').is(":checked");
+
+    let pizzaDeliveryFn=()=>{
+      if(delOpt === true){
+        return true
+      }
+      else{
+        return false
+      }
+    }
+
+    let pizzaDelivery=pizzaDeliveryFn()
+    let pizzaLocation
+    if(pizzaDelivery===true){
+        pizzaLocation=$('#delivery-location').val()
+    }
+    
+
+    console.log(pizzaDelivery);
+    console.log(pizzaToppings);
+    console.log(pizzaQuantity);
+    console.log(pizzaSize);
+    console.log(pizzaCrust);
+    console.log(pizzaLocation);
+  })
 })
